91973fff3038d1a0163827dc0de280ca17d3b6bd
Nikolay Stepanets <nik.stepanets@gmail.com>
2024-03-20 00:36:26 +0200

    added contrast settings for ST7567

--- yoRadio/src/core/display.cpp
+++ yoRadio/src/core/display.cpp
@@ -537,7 +537,7 @@ void Display::flip(){ dsp.flip(); }
 void Display::invert(){ dsp.invert(); }
 
 void  Display::setContrast(){
-  #if DSP_MODEL==DSP_NOKIA5110
+  #if (DSP_MODEL==DSP_NOKIA5110 || DSP_MODEL==DSP_ST7567 || DSP_MODEL==DSP_ST7567I2C)
     dsp.setContrast(config.store.contrast);
   #endif
 }
