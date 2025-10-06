52f69a4a3c5f8898ccfbac98bc125de370a4c6f1
Mykola Stepanets <nik.stepanets@gmail.com>
2025-10-06 12:08:42 +0300

    ESP32-vs1053_ext: fix m3u return value may be corrupt Commit 9b1f2f7

--- yoRadio/src/audioVS1053/audioVS1053Ex.cpp
+++ yoRadio/src/audioVS1053/audioVS1053Ex.cpp
@@ -1464,7 +1464,7 @@ const char* Audio::parsePlaylist_M3U(){
             break;
         }
     }
-    vector_clear_and_shrink(m_playlistContent);
+    // vector_clear_and_shrink(m_playlistContent);
     return host;
 }
 //----------------------------------------------------------------------------------------------------------------------
