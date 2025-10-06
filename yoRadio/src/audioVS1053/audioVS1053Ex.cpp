d2a479c1b960fd2b241b27c653792f515e4d1152
Mykola Stepanets <nik.stepanets@gmail.com>
2025-10-06 08:59:16 +0300

    ESP32-vs1053_ext: song doesn't stops correctly in stop_mp3Client() Commit b48054e

--- yoRadio/src/audioVS1053/audioVS1053Ex.cpp
+++ yoRadio/src/audioVS1053/audioVS1053Ex.cpp
@@ -4,7 +4,7 @@
  *  vs1053_ext.cpp
  *
  *  Created on: Jul 09.2017
- *  Updated on: Oct 20.2022
+ *  Updated on: Dec 05.2022
  *      Author: Wolle
  */
 #ifndef VS_PATCH_ENABLE
@@ -2190,8 +2190,10 @@ uint32_t Audio::stop_mp3client(){
         audiofile.close();
         setDatamode(AUDIO_NONE);
     }
+    stopSong();
     int v=read_register(SCI_VOL);
-    m_f_webstream=false;
+    m_f_webstream = false;
+    m_f_running = false;
     write_register(SCI_VOL, 0);                         // Mute while stopping
     #ifdef ESP_ARDUINO_3
     _client->clear();                                     // Flush stream client
