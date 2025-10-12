81afde119af32360efa50966e058b052d6a6f710
Mykola Stepanets <nik.stepanets@gmail.com>
2025-10-12 04:31:32 +0300

    fix m3u8 streamtitle wrong length audioI2S library, commit d73dd6a

--- yoRadio/src/audioVS1053/audioVS1053Ex.cpp
+++ yoRadio/src/audioVS1053/audioVS1053Ex.cpp
@@ -1688,9 +1688,8 @@ bool Audio::STfromEXTINF(char* str){
         if(t2 < 0 || t2 > t3) return false;
         n1 = t3 - t2;
         strncpy(m_chbuf + n0, str + t2, n1);
-        m_chbuf[n1] = '\0';
+        m_chbuf[n0 + n1] = '\0';
     }
-
     t1 = indexOf(str, "artist", 0);
     if(t1 > 0){
         strcpy(m_chbuf + n0 + n1, " - ");   n1 += 3;
@@ -1700,7 +1699,6 @@ bool Audio::STfromEXTINF(char* str){
         n2 = t3 - t2;
         strncpy(m_chbuf + n0 + n1, str + t2, n2);
         m_chbuf[n0 + n1 + n2] = '\0';
-        m_chbuf[n2] = '\0';
     }
     return true;
 }
