206876fe6161ac0882f54ae775960004093cf7fe
Mykola Stepanets <nik.stepanets@gmail.com>
2025-10-12 06:22:04 +0300

    fetch new playlist before the old one is empty audioI2S library, commit 4ae55c1

--- yoRadio/src/audioVS1053/audioVS1053Ex.cpp
+++ yoRadio/src/audioVS1053/audioVS1053Ex.cpp
@@ -1661,7 +1661,7 @@ const char* Audio::parsePlaylist_M3U8() {
         vector_clear_and_shrink(m_playlistContent); //clear after reading everything, m_playlistContent.size is now 0
     }
 
-    if(m_playlistURL.size() > 0){
+    if(m_playlistURL.size() > 1){
         if(m_playlistBuff) {free(m_playlistBuff); m_playlistBuff = NULL;}
 
         if(m_playlistURL[m_playlistURL.size() -1]) {
